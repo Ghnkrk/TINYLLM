ddb5364aa5c8a654ed31400d3de2c97b2ad4e423
Guhankarthik <164463894+Ghnkrk@users.noreply.github.com>
2025-12-14 13:56:12 +0530

    Update data.py

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 # Load config
 # -------------------------------------------------
 
-def load_config(path="config_10M.json"):
+def load_config(path="config_20M.json"):
     with open(path, "r") as f:
         return json.load(f)
 
@@ -106,5 +106,6 @@ def build_dataset(tokenizer, config_path="config.json"):
     )
 
     return LMDataset(input_ids, labels)
+
 
 
